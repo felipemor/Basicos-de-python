03e8c9f36ead47466d2a7f78614c0e306dd862d0
felipemor <77069159+felipemor@users.noreply.github.com>
2021-01-06 14:09:26 -0300

    Add files via upload

---
+++ teste02.py
@@ -0,0 +1,4 @@
+dia = input ('Que dia voce nasceu ?')
+mes = input ('Que mes voce nasceu ?')
+ano = input ('Que ano voce nasceu ?')
+print ('Olá Felipe estou te entregando a data formatada',dia,'/',mes,'/',ano)
---
+++ teste03.py
@@ -0,0 +1,4 @@
+a = input ('Qual o primeiro número ?')
+b = input ('Qual o segundo número ?')
+
+print ('Olá Felipe o resultado é ',int(a)+int(b))
